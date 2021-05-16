b81cd03821d0831c6456912ff406b1057ad75ded
Safwan Ahmed Salah <safwan@caspiands.com>
2021-05-16 11:59:14 +0800

    codemirror

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var htmlDom = document.getElementById('html');
 var buttonDom = document.getElementById('convert');
 var nameDom = document.getElementById('name');
-var resultNode = document.getElementById('result');
+var mirrorHtml = CodeMirror.fromTextArea(document.getElementById('html'), {mode:  "text/html", lineNumbers: true, lineWrapping: true });
+var mirrorPhp = CodeMirror(document.getElementById('result'), {mode:  "application/x-httpd-php", lineNumbers: true, readOnly:true, lineWrapping: true });
 var hasNoChild = ['img', 'link', 'input', 'meta'];
 
 buttonDom.onclick = function(){
     var el = document.createElement('res');
-    el.innerHTML = htmlDom.value.trim();
-    var component = nameDom.value || 'newComponent';
-    resultNode.innerText = `<?php
+    el.innerHTML = mirrorHtml.getValue().trim();
+    var component = (nameDom.value || 'newComponent').replace(/\W/g,'');
+    nameDom.value = component;
+    mirrorPhp.setValue(`<?php
 namespace React\\Tag;    
 use React\\Component;
 
@@ -16,18 +17,20 @@ class `+ component +` extends Component{
     function render(){
         return `+ toPhpReact(el.childNodes) +`;
     }
-}`;
+}`);
 }
 
 function toPhpReact(domList, tabs = 0){
     var doms = [], isList = domList.length > 1, space = isList ? '    '.repeat(tabs) : ''; 
     var html = isList ? '[\n' : '';
     for(var node of domList){
-        if(node.nodeType==3) doms.push(space +  "'" + node.data.trim() + "'");
-        else{
+        if(node.nodeType==3){ 
+            var txt = node.data.trim();
+            if(txt || !isList) doms.push(space +  '"' + txt + '"');
+        }else{
             var attributes = [], tag = node.tagName.toLowerCase();
             for(var att of node.attributes){
-                attributes.push("'" + att.name + "' => '" + att.value + "'" )
+                attributes.push('"' + att.name + '" => "' + att.value + '"')
             }
 
             doms.push(space + 'new ' + tag + '('+ (hasNoChild.includes(tag) ? '' : toPhpReact(node.childNodes, tabs+1)  + ', ') + '[' +  attributes.join(', ') +'])');
