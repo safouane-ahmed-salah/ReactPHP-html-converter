603d877fb09deb12abd713e3b4a2cf5ce9081d93
Safwan Ahmed Salah <safwan@caspiands.com>
2021-05-18 00:03:20 +0800

    fix issue with non html

--- script.js
+++ script.js
@@ -15,25 +15,26 @@ use React\\Component;
 
 class `+ component +` extends Component{
     function render(){
-        return `+ toPhpReact(el.childNodes) +`;
+        return `+ toPhpReact(el.childNodes).trim() +`;
     }
 }`);
 }
 
-function toPhpReact(domList, tabs = 0){
+function toPhpReact(domList, tabs = 2){
     var doms = [], isList = domList.length > 1, space = isList ? '    '.repeat(tabs) : ''; 
     var html = isList ? '[\n' : '';
     for(var node of domList){
         if(node.nodeType==3){ 
             var txt = node.data.trim();
             if(txt || !isList) doms.push(space +  '"' + txt + '"');
-        }else{
+        }else if(node.tagName){
             var attributes = [], tag = node.tagName.toLowerCase();
             for(var att of node.attributes){
                 attributes.push('"' + att.name + '" => "' + att.value + '"')
             }
-
-            doms.push(space + 'new ' + tag + '('+ (hasNoChild.includes(tag) ? '' : toPhpReact(node.childNodes, tabs+1)  + ', ') + '[' +  attributes.join(', ') +'])');
+            var content = toPhpReact(node.childNodes, tabs+1);
+            if(!content) content='null';
+            doms.push(space + 'new ' + tag + '('+ (hasNoChild.includes(tag) ? '' : content  + ', ') + '[' +  attributes.join(', ') +'])');
         }
     }
     html += doms.join(',\n');
